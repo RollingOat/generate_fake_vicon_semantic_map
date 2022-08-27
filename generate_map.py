d1b2abc7c0601311697bc1afe5a457d21436f88b
RollingOat <97129990+RollingOat@users.noreply.github.com>
2022-08-27 14:12:40 -0400

    Add files via upload

---
+++ generate_map.py
@@ -0,0 +1,25 @@
+import numpy as np
+import cv2
+
+class map{
+    def __init__(self, length, width, resolution){
+        '''
+        length: the length of vicon space in meter
+        width: the width of vicon space in meter
+        resolution: the size of cells in meter
+        '''
+        self.length = length
+        self.width = width
+        self.resolution = resolution
+    }
+
+    def generate_map(self,){
+
+    }
+
+    def add_object(self, shape, ){
+
+    }
+
+    def 
+}
